cb3c834e7152498e09867b69d95765bdf3527459
MacauleyStephenson <MacauleyStephenson@mail.com>
2023-02-08 10:14:12 +0000

    project resume

--- tell-the-weather/src/scripts/App.js
+++ tell-the-weather/src/scripts/App.js
@@ -16,6 +16,51 @@ import {
 } from '../assets/imports'
 class App extends React.Component {
 
+
+
+	constructor(props) {
+		super(props)
+		this.getData = this.getData.bind(this)
+		this.setLocation = this.setLocation.bind(this)
+		this.changeUnitMeasure = this.changeUnitMeasure.bind(this)
+		this.imagesID = {
+			'01d': sun, '02d': sCloud, '03d': cloud, '04d': cBroken, '09d': sShowerRain, '10d': sRain,
+			'11d': sElectricStorm, '13d': sSnow, '50d': sWind, '01n': moon, '02n': mCloud, '03n': cloud,
+			'04n': cBroken, '09n': mShowerRain, '10n': mRain, '11n': mElectricStorm, '13n': mSnow, '50n': mWind,
+		}
+		this.unitsOfMeasure = {
+			metric: {
+				tempUnit: '°C',
+				windSpeedUnit: 'm/s',
+			},
+			imperial: {
+				tempUnit: '°F',
+				windSpeedUnit: 'm/h',
+			}
+		}
+		this.state = {
+			location: 'salta, argentina',
+			info: {
+				state: '',
+				country: '',
+				weatherImage: '',
+				description: '',
+				feels_like: '',
+				humidity: '',
+				pressure: '',
+				temp: '',
+				temp_max: '',
+				temp_min: '',
+				visibility: '',
+				wind_speed: '',
+				uvi: '',
+			},
+			daily: [],
+			hourly: [],
+			unitOfMeasure: 'metric'
+		}
+	}
+
 	render() {
 
 		return (
