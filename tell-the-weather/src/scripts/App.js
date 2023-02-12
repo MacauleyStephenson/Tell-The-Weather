158ea66cfc72aad2b9c10bf8ebe7ca5896f0e829
MacauleyStephenson <MacauleyStephenson@mail.com>
2023-02-12 10:51:46 +0000

    completed any other remaining components

--- tell-the-weather/src/scripts/App.js
+++ tell-the-weather/src/scripts/App.js
@@ -16,8 +16,6 @@ import {
 } from '../assets/imports'
 class App extends React.Component {
 
-
-
 	constructor(props) {
 		super(props)
 		this.getData = this.getData.bind(this)
@@ -61,6 +59,73 @@ class App extends React.Component {
 		}
 	}
 
+	componentDidMount() {
+		this.getData()
+	}
+
+	changeUnitMeasure(unit) {
+		this.setState({
+			unitOfMeasure: unit
+		})
+		setTimeout(() => {
+			console.log(this.state.unitOfMeasure)
+		}, 0);
+	}
+
+	async getData(e) {
+		try {
+			e?.preventDefault?.()
+			const geoCodingRequest = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.state.location}&limit=1&appid=11c5983b7ba6619286736eb3da4d20cc`);
+			const geoCodingJson = await geoCodingRequest.json();
+			const weatherDataRequest = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${geoCodingJson[0].lat}&lon=${geoCodingJson[0].lon}&exclude={part}&appid=11c5983b7ba6619286736eb3da4d20cc&units=${this.state.unitOfMeasure}`);
+			const weatherDataJson = await weatherDataRequest.json();
+			weatherDataJson.daily.forEach((day) => {
+				day.uniqid = uniqid()
+				day.temp.min = `${Math.round(day.temp.min)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				day.temp.max = `${Math.round(day.temp.max)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				day.temp.day = `${Math.round(day.temp.day)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				day.temp.night = `${Math.round(day.temp.night)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				day.wind_speed = `${day.wind_speed} ${this.unitsOfMeasure[this.state.unitOfMeasure].windSpeedUnit}`
+			})
+			weatherDataJson.hourly.forEach((hour) => {
+				hour.uniqid = uniqid()
+				hour.temp = `${Math.round(hour.temp)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				hour.feels_like = `${Math.round(hour.feels_like)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				hour.dew_point = `${Math.round(hour.dew_point)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`
+				hour.visibility = `${hour.visibility} m`
+				hour.wind_speed = `${hour.wind_speed} ${this.unitsOfMeasure[this.state.unitOfMeasure].windSpeedUnit}`
+				hour.pop = `${hour.pop * 100}%`
+			})
+			this.setState({
+				info: {
+					state: geoCodingJson[0].state,
+					country: geoCodingJson[0].country,
+					temp: `${Math.round(weatherDataJson.current.temp)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`,
+					wind_speed: `${weatherDataJson.current.wind_speed}${this.unitsOfMeasure[this.state.unitOfMeasure].windSpeedUnit}`,
+					weatherImage: this.imagesID[weatherDataJson.current.weather[0].icon],
+					description: weatherDataJson.current.weather[0].main,
+					feels_like: `${Math.round(weatherDataJson.current.feels_like)}${this.unitsOfMeasure[this.state.unitOfMeasure].tempUnit}`,
+					humidity: weatherDataJson.current.humidity,
+					pressure: weatherDataJson.current.pressure,
+					uvi: weatherDataJson.current.uvi,
+					visibility: weatherDataJson.current.visibility,
+				},
+				daily: weatherDataJson.daily,
+				hourly: weatherDataJson.hourly,
+			})
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	setLocation(e) {
+		const inputForm = document.querySelector('#inputForm')
+		this.setState({
+			location: inputForm.value
+		})
+	}
+
+
 	render() {
 
 		return (
@@ -69,7 +134,7 @@ class App extends React.Component {
 				<AnimatePresence>
 					{!!this.state.daily.length ? <>
 						<div className="appContainer">
-							<a className='github' href="https://github.com/MacauleyStephenson/Tell-The-Weather">
+							<a className='github' href="https://github.com/Apheiro/weather-app">
 								<IconSquareGithub width='50px' height='50px' />
 								<h2>Apheiros</h2>
 							</a>
@@ -81,10 +146,13 @@ class App extends React.Component {
 					</> : <LoadingScreen />
 					}
 				</AnimatePresence>
+
+
+
 			</div>
 		);
 	}
-}
 
+}
 
 export default App;
---
+++ tell-the-weather/src/scripts/PopupInfo.js
@@ -0,0 +1,107 @@
+import react from 'react'
+import '../styles/PopupInfo.css'
+import uniqid from 'uniqid'
+import { fromUnixTime, format } from 'date-fns'
+import {
+	cloud, sun, sCloud,
+	sRain, sSnow, sElectricStorm,
+	sWind, moon, mCloud, mRain, mSnow,
+	mElectricStorm, mWind, cBroken,
+	sShowerRain, mShowerRain,
+} from '../assets/imports'
+import {
+	IconWiHot,
+	IconWiSunrise, IconWiSunset,
+	IconMoon, IconSun, IconWiRain, IconWiCloud,
+	IconEye, IconWiThermometerExterior
+} from '../assets/imports'
+import { motion } from 'framer-motion'
+
+class PopupInfo extends react.Component {
+	constructor(props) {
+		super(props)
+		this.imagesID = {
+			'01d': sun, '02d': sCloud, '03d': cloud, '04d': cBroken, '09d': sShowerRain, '10d': sRain,
+			'11d': sElectricStorm, '13d': sSnow, '50d': sWind, '01n': moon, '02n': mCloud, '03n': cloud,
+			'04n': cBroken, '09n': mShowerRain, '10n': mRain, '11n': mElectricStorm, '13n': mSnow, '50n': mWind,
+		}
+		this.initial = {
+			scale: 0.6,
+			opacity: 0,
+		}
+		this.enter = {
+			scale: 1,
+			opacity: 1,
+			transition: {
+				duration: 0.4,
+				ease: [0.60, -0.67, 0.30, 1.67]
+			}
+		}
+		this.exit = {
+			scale: 0.6,
+			opacity: 0,
+			transition: {
+				duration: 0.4,
+				ease: [0.60, -0.67, 0.30, 1.67]
+			}
+		}
+	}
+
+	render() {
+		const { uniqKey, formatDate, formatDateType } = this.props;
+		return (
+			< motion.div className='popupInfo' onClick={this.props.removePopup} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+				{
+					formatDate.map((element) => {
+						if (formatDateType == 'daily') {
+							if (element.uniqid == uniqKey) {
+								return (
+									<motion.div key={uniqid()} className='moreInfoCard' initial={this.initial} animate={this.enter} exit={this.exit}>
+										<div className='basicInfo'>
+											<h2>{format(fromUnixTime(element.dt), 'PPPP')}</h2>
+											<img src={this.imagesID[element.weather[0].icon]} alt="" />
+											<p>{element.temp.min}-{element.temp.max}</p>
+											<h2>{element.weather[0].description.toUpperCase()}</h2>
+										</div>
+										<div className='moreInfo'>
+											<h2>info</h2>
+											<p><IconWiSunrise />sunrise: {format(fromUnixTime(element.sunrise), 'hh:mm a')}</p>
+											<p><IconWiSunset />sunset: {format(fromUnixTime(element.sunset), 'hh:mm a')}</p>
+											<p><IconSun />temp day: {element.temp.day}</p>
+											<p><IconMoon />temp night: {element.temp.night}</p>
+											<p><IconWiHot />uv index: {element.uvi}</p>
+										</div>
+									</motion.div>
+								)
+							}
+						} else if (formatDateType == 'hourly') {
+							if (element.uniqid == uniqKey) {
+								return (
+									<motion.div key={uniqid()} className='moreInfoCard' initial={this.initial} animate={this.enter} exit={this.exit}>
+										<div className='basicInfo'>
+											<h2>{format(fromUnixTime(element.dt), 'hh:mm a')}</h2>
+											<img src={this.imagesID[element.weather[0].icon]} alt="" />
+											<p>{element.temp}</p>
+											<h2>{element.weather[0].description.toUpperCase()}</h2>
+										</div>
+										<div className='moreInfo'>
+											<h2>info</h2>
+											<p><IconWiThermometerExterior />feels like: {element.feels_like}</p>
+											<p><IconEye />visibility: {element.visibility}</p>
+											<p><IconWiCloud />atmoshpehric temp: {element.dew_point}</p>
+											<p><IconWiRain></IconWiRain> precipitation: {element.pop}</p>
+											<p><IconWiHot />uv index: {element.uvi}</p>
+										</div>
+									</motion.div>
+								)
+							}
+						}
+					})
+				}
+			</motion.div >
+		)
+
+	}
+}
+
+export default PopupInfo
